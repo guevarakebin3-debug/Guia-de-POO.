30031af69df2c6e94c22fb38f74e7ba1a038fadf
guevarakebin3-debug <guevarakebin3@gmail.com>
2026-05-28 21:23:15 -0500

    Update routes.py

--- app/blueprints/bloque21/routes.py
+++ app/blueprints/bloque21/routes.py
@@ -1,1 +1,8 @@
+from flask import Blueprint, render_template
+
+bloque21_bp = Blueprint("bloque21", __name__, template_folder="templates")
+
+@bloque21_bp.route("/")
+def home():
+    return render_template("bloque21.html")
 
